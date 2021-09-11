5758f3c01eb6fee4e9e11c5ad58a6f0158b7f002
Abdulkareem1094 <abdulak4t7@gmail.com>
2021-09-11 18:24:58 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const App = () => {
     <div className='tc'>
       <h1 className='tc'>RoboFriends</h1>
       <SearchBox />
+      <SearchBox />
+      <SearchBox />
       <CardList robots={robots} />
     </div>
   );
